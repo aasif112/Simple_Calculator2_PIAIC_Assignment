c23ce8cd69399dcbdf47149bbaf638db874e2c70
aasif112 <156135474+aasif112@users.noreply.github.com>
2024-06-24 22:10:48 +0500

    updating after transpiling by tsc command

--- index.ts
+++ index.ts
@@ -14,7 +14,6 @@ const answer = await inquirer.prompt([
     }
 ])
 
-
 if(answer.operator === "Add") {
     console.log(`${answer.firstNumber} + ${answer.secondNumber} = ${answer.firstNumber+answer.secondNumber}`)
 }
